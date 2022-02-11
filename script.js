dbcef295593168a3c7d4bfe7475ffbb4a2a1b26a
znk21 <vincentbruna.oficio@gmail.com>
2022-02-11 15:19:03 -0300

    Funcionalidad completa

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 $(function () {
-    $("input").click(function(){ 
-        $(".display").attr("value", $(this).val())
+    var num1 = "";
 
+
+    $("input").not(".display").click(function() {
+        num1 = num1 + $(this).val();
+        $(".display").val(num1)
+
+    });
+    $("#igual").click(function(){
+        var resultado = eval(num1);
+        $(".display").val(resultado);
+
+    });
+    $("#borrar").click(function(){ 
+        $(".display").val("");
+        num1 = ""
     });
 });
